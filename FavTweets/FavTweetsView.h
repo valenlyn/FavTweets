5556d25dcd97917f7dbe3ac7384a48b3c3fc1f46
valenlyn <valenlynchua@gmail.com>
2024-05-16 23:18:07 +0800

    initial commit

---
+++ FavTweets/FavTweetsView.h
@@ -0,0 +1,12 @@
+//
+//  FavTweetsView.h
+//  FavTweets
+//
+//  Created by Valenlyn Chua on 16/5/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface FavTweetsView : ScreenSaverView
+
+@end
---
+++ FavTweets/FavTweetsView.m
@@ -0,0 +1,51 @@
+//
+//  FavTweetsView.m
+//  FavTweets
+//
+//  Created by Valenlyn Chua on 16/5/24.
+//
+
+#import "FavTweetsView.h"
+
+@implementation FavTweetsView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
